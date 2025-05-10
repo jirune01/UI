395a0a7a66426ebcedd062685c2f7f37a68b020c
jihun <jhmc5268@naver.com>
2025-05-10 17:03:31 +0900

    Chore: Code component layout Add

--- src/components/Code.tsx
+++ src/components/Code.tsx
@@ -1,26 +1,31 @@
-import { FaRegCopy } from "react-icons/fa";
+import { SettingIcon } from "assets/icons/svg/SettingIcon";
+import "styles/code.css";
+
 export const Code = () => {
   return (
-    <div className='w-85 h-200 p-2 mr-2 rounded-sm' 
-        style={{backgroundColor: "#282828"}}>
-        <div className='flex p-4 h-10 items-center justify-between '>
-            <div className='flex gap-1.5'>
-                <div className='w-2.5 h-2.5 rounded-full'
-                style={{backgroundColor: "#F5655B"}}/>
-                <div className='w-2.5 h-2.5 rounded-full'
-                style={{backgroundColor: "#F6BD3B"}}/>
-                <div className='w-2.5 h-2.5 rounded-full'
-                style={{backgroundColor: "#43C645"}}/>
-            </div>
-            {/* setting Icon */}
-            <div>
-                {/* Tooltip 추가 필요 */}
-              <FaRegCopy fill="#FFF" className="cursor-pointer"/>
-            </div>
+    <div className="code-container">
+      <div className="code-header">
+        <div className="flex gap-1.5">
+          <div
+            className="code-header-circle"
+            style={{ backgroundColor: "#F5655B" }}
+          />
+          <div
+            className="code-header-circle"
+            style={{ backgroundColor: "#F6BD3B" }}
+          />
+          <div
+            className="code-header-circle"
+            style={{ backgroundColor: "#43C645" }}
+          />
         </div>
-            
+        <div>
+          <SettingIcon />
+        </div>
+      </div>
 
-    
-  </div>
-  )
-}
+      {/* code  */}
+      <div className="code-main custom-view-scrollbar">123</div>
+    </div>
+  );
+};
