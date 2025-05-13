730433d95f41b131fcc8553fdf2c2ae7e83af12e
jirune <jhmc5268@naver.com>
2025-05-13 09:51:12 +0900

    Chore: ArrowDown svg Add

---
+++ src/assets/icons/svg/ArrowDown.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export const ArrowDown = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="32"
+      viewBox="0 0 100 100"
+    >
+      <rect width="32" height="32" rx="8" ry="8" fill="transparent" />
+      <path
+        d="M30 40 L50 60 L70 40"
+        fill="none"
+        stroke="#ccc"
+        stroke-width="8"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
