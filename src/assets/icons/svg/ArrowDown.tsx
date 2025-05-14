e43168cab6affaf68e50107ce3565c99ff3c1c07
jirune <jhmc5268@naver.com>
2025-05-14 10:43:54 +0900

    Chore: Svg Icon Add

--- src/assets/icons/svg/ArrowDown.tsx
+++ src/assets/icons/svg/ArrowDown.tsx
@@ -1,21 +1,29 @@
 import React from "react";
 
-export const ArrowDown = () => {
+interface ArrowDownProps {
+  size?: number;
+  color?: string;
+}
+
+export const ArrowDown: React.FC<ArrowDownProps> = ({
+  size = 32,
+  color = "#ccc",
+}) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="32"
-      height="32"
+      width={size}
+      height={size}
       viewBox="0 0 100 100"
     >
-      <rect width="32" height="32" rx="8" ry="8" fill="transparent" />
+      <rect width="100" height="100" rx="8" ry="8" fill="transparent" />
       <path
         d="M30 40 L50 60 L70 40"
         fill="none"
-        stroke="#ccc"
-        stroke-width="8"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        stroke={color}
+        strokeWidth={6}
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
